53c4b2c874a7100d641d18b2ded3e89271a7f0fe
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2017-01-04 22:47:04 +0100

    Fix MissingMethodErrorObserver for Behat 3

--- src/Extension/PhpSpecExtension/src/ErrorObserver/MissingMethodErrorObserver.php
+++ src/Extension/PhpSpecExtension/src/ErrorObserver/MissingMethodErrorObserver.php
@@ -2,6 +2,7 @@
 
 namespace RMiller\PhpSpecExtension\ErrorObserver;
 
+use Behat\Testwork\Call\Exception\FatalThrowableError;
 use RMiller\ErrorExtension\Observer\ErrorObserver;
 use RMiller\PhpSpecExtension\Process\ExemplifyRunner;
 
@@ -14,14 +15,15 @@ class MissingMethodErrorObserver implements ErrorObserver
         $this->exemplifyRunner = $exemplifyRunner;
     }
 
-    public function notify(array $error)
+    public function notify(FatalThrowableError $error)
     {
-        if (strpos($error['message'], 'Call to undefined method') === false) {
+        if (strpos($error->getMessage(), 'Call to undefined method') === false) {
             return;
         }
 
-        $missing = trim(substr($error['message'], strlen('Call to undefined method')));
-        list ($class, $method) = explode('::', $missing);
+        $missing = trim(substr($error->getMessage(), strlen('Fatal error: Call to undefined method')));
+        list($class, $method) = explode('::', $missing);
+
         $this->exemplifyRunner->runExemplifyCommand($class, substr($method, 0, -2));
     }
 }
